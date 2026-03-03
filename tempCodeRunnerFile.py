8ff4b843bf24a00c02870201a95f6c2d3802aa7a
Okomekomekome12 <okomekomekome1@outlook.com>
2026-03-03 04:30:49 +0000

    deleted:    a.py 	modified:   antispam.py 	deleted:    server.py 	deleted:    tempCodeRunnerFile.py 	deleted:    test.py

--- tempCodeRunnerFile.py
+++
@@ -1,3 +0,0 @@
-import chatwork1
-cw = chatwork1.setup("aaa","jljlk")
-cw.messagesend("test")
--- test.py
+++
@@ -1,5 +0,0 @@
-import chatwork
-cw = chatwork.setup("423252546","apitoken")
-cw.messagesend("test")
-cw.member_remove(10839722)
-cw.get_message_link()
