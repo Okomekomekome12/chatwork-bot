ebf0cc5e94157c55a9488b6850e59344b80102b3
Okomekomekome12 <okomekomekome1@outlook.com>
2026-04-28 20:54:39 +0900

    new file:   .ipynb_checkpoints/chatwork-checkpoint.py 	new file:   .ipynb_checkpoints/main-checkpoint.py

---
+++ .ipynb_checkpoints/main-checkpoint.py
@@ -0,0 +1,47 @@
+import os
+import time
+import chatwork
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+API_TOKEN = os.getenv("API_TOKEN")
+SECRET_TOKEN = None
+
+@app.route("/", methods=["GET"])
+def health():
+    return "OK"
+
+@app.route("/webhook", methods=["POST"])
+def webhook():
+    signature = request.headers.get("X-ChatWorkWebhookSignature")
+    if not chatwork.webhook_verify_signature(request.data, signature, SECRET_TOKEN): # type: ignore
+        return "invalid signature", 403
+    data = request.json
+    room_id    = chatwork.webhook_get_roomid(data) # type: ignore
+    body       = chatwork.webhook_get_message(data)
+    account_id = chatwork.webhook_get_account_id(data) # type: ignore
+    message_id = chatwork.webhook_get_message_id(data)
+
+    print(f"ルームID: {room_id}")
+    print(f"送信者ID: {account_id}")
+    print(f"メッセージ: {body}")
+
+    cw = chatwork.setup(room_id, API_TOKEN)
+    cw2 = chatwork.setup(420107748,API_TOKEN)
+    if body and (body.count("(quick)") >= 10 or body.count(":*") >= 10):
+        cw.viewer(account_id)
+        cw.messagesend("[info][title]荒らし検知[/title]荒らしを検知しました、流します[/info]")
+        for i in range(50):
+            cw.messagesend("a")
+            time.sleep(0.7)
+        message_link = cw.get_message_link()
+        cw.messagesend("[info][title]荒らし対処完了[/title]メッセージリンクを配布します[/info]")
+        cw2.messagesend(f"[info][title]メッセリンク配布[/title]{message_link}[/info]")
+    elif body == "/live?":
+        cw.messagesend("[info][title]荒らし対策bot正常稼働中[/title]生きてるお[/info]")
+    return jsonify({"status": "ok"}), 200
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
